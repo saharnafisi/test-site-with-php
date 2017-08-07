2f8e37f2ee90755c770103c2ab8eaf0ad78c42b0
Sahar Nafisi <sahar.nafisi95@gmail.com>
2017-08-07 11:39:50 +0430

    bugs in login fixed

--- selectUser.php
+++ selectUser.php
@@ -7,18 +7,26 @@
 
         exit();
     }
-    Redirect("http://localhost/mini_project/main.php");
+
     $username=$_POST["userName"];
     $password=$_POST["password"];
     try{
-        $stmt=$conn->prepare("SELECT * FROM `user` WHERE `userName`='$username' and `password`='$password';");
-        $stmt->execute();
+        //$stmt=$conn->prepare("SELECT * FROM `user` WHERE `userName`='$username' and `password`='$password';");
+        $stmt=$conn->query("SELECT * FROM `user` WHERE `userName`='$username' and `password`='$password';");
+        $result=$stmt->fetch();
+        if($result["name"]!=""){
+            echo "خوش امدید";
+            $_SESSION["login"]=true;
+            $_SESSION["username"]=$username;
+        }
+        else{
+            echo "نام کاربری یا رمز عبور صحیح نمیباشد";
+        }
     }
     catch(PDOException $ex){
         echo $command . "<br>" . $ex->getMessage();
     }
     /*Redirect("http://localhost/mini_project/main.php");
-    echo "کاربر با موفقیت افزوده شد";
     $_SESSION["login"]=true;
     $_SESSION["username"]=$userName;
     echo $_SESSION["login"];
